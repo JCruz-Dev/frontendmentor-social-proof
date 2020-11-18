3d36893fc9c90f63ae4abacbdfbd6ce8ffae6958
Juan carlos <juancarloscruz278@gmail.com>
2020-11-17 20:31:41 -0400

    Feature: Initial Tailwind config.

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: ['./**/*.html'],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: '2rem'
+    }
+  },
+  variants: {},
+  plugins: [],
+}
